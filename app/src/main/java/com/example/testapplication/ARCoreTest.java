33d3500077714c866d8e870d61e54314ab16482a
Kumail Kamran <kumailhaider360@gmail.com>
2019-10-24 07:12:01 +0100

    - AR skeleton fixed for rendering magikarp

--- app/src/main/java/com/example/testapplication/ARCoreTest.java
+++ app/src/main/java/com/example/testapplication/ARCoreTest.java
@@ -42,7 +42,7 @@ public class ARCoreTest extends AppCompatActivity {
         // When you build a Renderable, Sceneform loads its resources in the background while returning
         // a CompletableFuture. Call thenAccept(), handle(), or check isDone() before calling get().
         ModelRenderable.builder()
-                .setSource(this, Uri.parse(R.class.getResource("magikarpmodel.sfb").toExternalForm()))
+                .setSource(this, R.raw.magikarpmodel)
                 .build()
                 .thenAccept(renderable -> andyRenderable = renderable)
                 .exceptionally(
