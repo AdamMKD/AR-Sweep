67ea048017b27860364236e8edce3cf219b78c64
Haashim <32544671+stravos97@users.noreply.github.com>
2019-10-26 21:41:36 +0100

    a
    360 degree

--- app/src/main/java/com/example/testapplication/MainActivity.java
+++ app/src/main/java/com/example/testapplication/MainActivity.java
@@ -3,15 +3,66 @@ package com.example.testapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
 import androidx.appcompat.app.AppCompatActivity;
+import android.animation.ValueAnimator;
+import android.media.Image;
+import android.renderscript.Sampler;
+import android.view.animation.LinearInterpolator;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button playButton;
+    private Button exitButton;
+
+    //Used for panorama on home screen
+    private ImageView backgroundOne;
+    private ImageView backgroundTwo;
+    final ValueAnimator animator = ValueAnimator.ofFloat(0.0f, 1.0f);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        backgroundOne = (ImageView) findViewById(R.id.background1);
+        backgroundTwo = (ImageView) findViewById(R.id.background2);
+
+
+        playButton = (Button) findViewById(R.id.playButton);
+
+        exitButton = findViewById(R.id.exit);
+
+        //exitButton exit game
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
+
+    animate(animator);
+    }
+
+    private void animate(ValueAnimator animator){
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setInterpolator(new LinearInterpolator()); //what is this
+        animator.setDuration(10000L);
+
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                final float progress = (float) animation.getAnimatedValue();
+                final float width = backgroundOne.getWidth();
+                final float translationX = width * progress;
+                backgroundOne.setTranslationX(translationX);
+                backgroundTwo.setTranslationX(translationX - width);
+            }
+        });
+
+        animator.start();
     }
 
 
